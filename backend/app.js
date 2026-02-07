ac65ce0985909df03eebd6efef88228638db9398
vashukyadav <vashuuyadav08@gmail.com>
2026-02-07 11:01:41 +0530

    add fronted url to .env

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,12 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
