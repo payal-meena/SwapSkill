9890126f77cdcb9aa052a5cc89511906d5bd021d
payal-meena <payalm.bca2023@ssism.org>
2026-02-10 16:28:56 +0530

    path mismatch problem

--- frontend/src/layouts/UserLayout.jsx
+++ frontend/src/layouts/UserLayout.jsx
@@ -1,29 +1,42 @@
-import React, { useState } from 'react';
+// import React from 'react';
+// import { Outlet } from 'react-router-dom';
+// import UserSidebar from '../components/common/UserSidebar';
+// import UserNavbar from '../components/common/UserNavbar';
+
+// const UserLayout = () => {
+//   return (
+//     <div className="flex h-screen overflow-hidden">
+//       <UserSidebar />
+//       <div className="flex-1 flex flex-col overflow-hidden">
+
+//       <UserNavbar/>
+      
+//       <main className="flex-1 overflow-y-auto bg-background-light dark:bg-background-dark">
+//         <Outlet /> 
+//       </main>
+//         </div>
+//     </div>
+//   );
+// };
+
+// export default UserLayout;
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import UserSidebar from '../components/common/UserSidebar';
 import UserNavbar from '../components/common/UserNavbar';
 
 const UserLayout = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
   return (
-    <div className="flex h-screen overflow-hidden ">
-
-      {/* SIDEBAR */}
-      <UserSidebar
-        isOpen={isSidebarOpen}
-        closeSidebar={() => setIsSidebarOpen(false)}
-      />
-
-      {/* RIGHT CONTENT */}
+    <div className="flex h-screen overflow-hidden">
+      <UserSidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
-        <UserNavbar openSidebar={() => setIsSidebarOpen(true)} />
 
-        <main className="flex-1 overflow-y-auto scrollbar-hide bg-background-light dark:bg-background-dark">
-          <Outlet />
-        </main>
-      </div>
-
+      <UserNavbar/>
+      
+      <main className="flex-1 overflow-y-auto scrollbar-hide bg-background-light dark:bg-background-dark">
+        <Outlet /> 
+      </main>
+        </div>
     </div>
   );
 };
